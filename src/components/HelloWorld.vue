5334a2d2742e16fa4dec0a979c484ba288e4f5d2
Unknown <hidorabmon@gmail.com>
2018-05-03 17:04:16 +0800

    Initial commit
    test branch

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>Wellcome Page</h1>
+    <md-card>
+      <md-card-media-cover md-solid>
+        <md-card-media md-ratio="1:1">
+          <img src="/assets/examples/card-sky.jpg" alt="Skyscraper">
+        </md-card-media>
+
+        <md-card-area>
+          <md-card-header>
+            <span class="md-title">Solid background</span>
+            <span class="md-subhead">1/1 image</span>
+          </md-card-header>
+
+          <md-card-actions>
+            <md-button class="md-icon-button">
+              <md-icon>favorite</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+              <md-icon>share</md-icon>
+            </md-button>
+          </md-card-actions>
+        </md-card-area>
+      </md-card-media-cover>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- 这里设置中部div的样式 -->
+<style lang="scss" scoped>
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
---
+++ src/components/count.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+    <h2>count:<span class="count"> {{ count }}</span></h2>
+    Set Number :
+    <input type="number" style="width:50px;" v-model="inputNumber"><br/>
+    <md-button class="md-dense md-primary"
+    @click="actionIncrease(inputNumber)">
+     + {{inputNumber}}
+    </md-button>
+    <md-button class="md-dense md-raised md-primary"
+    @click="actionDecrease(inputNumber)">
+     - {{inputNumber}}
+    </md-button>
+    <br/>
+    <md-button class="md-raised" @click="actionReset">Reset</md-button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      inputNumber: 1
+    };
+  },
+  computed: {
+    ...mapGetters({
+      count: "getCount"
+    })
+  },
+  methods: {
+    ...mapActions(["actionIncrease", "actionDecrease", "actionReset"])
+  }
+};
+</script>
